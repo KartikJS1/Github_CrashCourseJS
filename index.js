0c026cff94f35464c272d6429eff6b2735798843
KartikRC1 <kartikrameshchavan2003@gmail.com>
2024-11-11 19:20:07 +0530

    code reverted

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const age = 21;
 
-const name1 = "KartikJS";
+const name = "KartikJS";
 
 const dob = "22/08/2003";
 
 function myName() {
-    console.log(name1);
+    console.log(name);
 }
 myName();
 
-console.log("HiiJS");
 
-
-console.log("HelloJS");
